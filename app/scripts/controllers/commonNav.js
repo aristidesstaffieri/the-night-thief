d523bcf5e5c85d783d901b266bebec0f41f04222
Aristides Staffieri <aristides@aristidesstaffieri.com>
2015-02-22 13:50:34 -0700

    Added states

--- app/scripts/controllers/commonNav.js
+++ app/scripts/controllers/commonNav.js
@@ -8,21 +8,27 @@
  * Controller of the theNightThiefApp
  */
 
-app.controller('commonNavCtrl', ['$scope', '$log', '$location', 'Auth',
-	function($scope, $log, $location, Auth) {
-		$log.debug(Auth);
+ app.controller('commonNavCtrl', ['$scope', '$log', '$location', 'Auth',
+ 	function($scope, $log, $location, Auth) {
+ 		$log.debug(Auth);
 	// $rootScope.loggedIn = currentAuth;
 
 
 	Auth.auth.$onAuth(function(authData) {
-     $scope.authData = authData;
-  });
+		$scope.authData = authData;
+	});
 
 
 	$scope.logout = function () {
-	    		Auth.logout();
-	    		$location.path('/login');
-	    	};
+		Auth.logout();
+		$location.path('/login');
+	};
+
+	$scope.isActive = function(route) {
+		console.log($location.path());
+		console.log(route === $location.path())
+		return route === $location.path();
+	}
 
 }]);
 
